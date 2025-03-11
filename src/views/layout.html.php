530a1e3a53009f017f42ad13bbe0c735e3e07268
kaka1409 <khanhphqgcs230461@fpt.edu.vn>
2025-03-11 14:20:39 +0700

    update 3

--- src/views/layout.html.php
+++ src/views/layout.html.php
@@ -8,22 +8,45 @@
     <title>forum</title>
 </head>
 <body>
+    <!-- HEADER -->
     <header>
-        <h1 class="logo">Forum</h1>
+        <h1 class="logo">FORUM</h1>
 
         <div class="search_bar">
-            <div class="seach_icon"></div>
-            <input type="text" placeholder="Search posts or users">
+            <div class="search_icon">
+                <img src="../public/assets/icons/search_icon.png" alt="">
+            </div>
+            <input id="search_input" type="text" placeholder="Search posts or users">
         </div>
 
         <div class="theme_switcher"></div>
 
-        <div class="profile_container">profile</div>
+        <div class="profile_container">
+            <div class="profile_name">
+                username
+            </div>
+            <div class="profile_avatar">
+                <img 
+                    width="40px"
+                    height="40px"
+                    src="https://avatars.githubusercontent.com/u/172468139?v=4" 
+                    alt=""
+                >
+            </div>
+        </div>
     </header>
 
+    <!-- MAIN -->
     <main class="main_container">
+
+        <!-- SIDEBAR -->
         <aside class="side_menu">side menu</aside>
-        <main class="main_content">main</main>
+
+        <!-- MAIN CONTENT -->
+        <main class="main_content">
+            <!-- Post listing here -->
+             main
+        </main>
     </main>
 
 </body>
