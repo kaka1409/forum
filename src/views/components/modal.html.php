f5af0f4058adbef06a18a5e3906a492e77b4bbc6
kaka1409 <khanhphqgcs230461@fpt.edu.vn>
2025-03-22 16:27:35 +0700

    update 43 (fixing modal a little bit)

--- src/views/components/modal.html.php
+++ src/views/components/modal.html.php
@@ -1,5 +1,6 @@
 <div class="modal">
     <div class="pop_up">
+        <h1>Don't have an account?</h1>
         <p>To use this feature you need to have an account</p>
         <img src="<?=BASE_URL?>assets/icons/exit.png" alt="">
         <a href="<?=BASE_URL?>login">Sign in</a>
